0fc45a1d6eb4f295573865f1cc0bc7109a1d9acc
zhuzhupsen <zhuzhaka.dog@gmail.com>
2023-09-22 01:37:33 +0500

    added droplist as standalone component

--- src/components/DropList/index.jsx
+++ src/components/DropList/index.jsx
@@ -1,51 +1,67 @@
-import { useState } from "react";
 import "./DropList.scss";
 
 // items = [{ title, icon?, dataAttributes?: {'attribute': "..."} }]
 
+const listWrapperClassName = "dropdown-list__items-wrapper";
+const listDroppedClassName = "dropdown-list__items-wrapper_dropped";
+
+function closeDropListsHandler(e) {
+  if (!e.target.closest('.dropdown-list')) {
+    document.querySelectorAll('.dropdown-list__items-wrapper_dropped')
+    .forEach(item => item.classList.remove('dropdown-list__items-wrapper_dropped'));
+    
+    window.removeEventListener('click', closeDropListsHandler, false);
+    console.log('window event')
+  }
+}
+
 export const DropList = ({
   items,
-  selectedItem,
+  selectedItemTitle,
   onItemClick,
   onListDropped,
 }) => {
-  const [listDropped, setListDropped] = useState(false);
-
-  const onListDroppedHandler = (e) => {
-    setListDropped(true);
-
-    if (typeof onListDropped === "function") {
-      onListDropped(e);
-    }
-  };
-
-  const onItemClickHandler = (e) => {
-    setListDropped(false);
-
+  const selectedItem = items.find((item) => item.title === selectedItemTitle);
+ 
+  const onItemClickHandler = (e) => { 
     if (typeof onItemClick === "function") {
       onItemClick(e);
     }
   };
 
+  const onListDroppedHandler = (e) => {
+    const [currentElement] = e.currentTarget.querySelectorAll('.' + listWrapperClassName);
+    const isDropped = currentElement.classList.contains(listDroppedClassName);
+    
+    document.querySelectorAll('.' + listDroppedClassName)
+      .forEach(element =>
+        currentElement !== element && element.classList.remove(listDroppedClassName)
+      );
+      
+    currentElement.classList.toggle(listDroppedClassName);
+    
+    if (isDropped) {
+      window.removeEventListener('click', closeDropListsHandler, false);
+    } else {
+      window.addEventListener('click', closeDropListsHandler, false);
+    }
+
+    if (typeof onListDropped === "function") {
+      onListDropped(e);
+    }    
+  }
+
   return (
-    <div className="dropdown-list">
-      <div
-        className={`dropdown-list__overlay${
-          listDropped ? " dropdown-list__overlay_dropped" : ""
-        }`}
-        onClick={() => setListDropped(false)}
-      ></div>
-      <div
-        className="dropdown-list__preview-item"
-        onClick={onListDroppedHandler}
-      >
-        {items.find((item) => item.title === selectedItem).icon()}
-        <span>{selectedItem}</span>
+    <div
+      className="dropdown-list"     
+      onClick={onListDroppedHandler}
+    >
+      <div className="dropdown-list__preview-item">
+        {selectedItem.icon ? (typeof selectedItem.icon === 'function' ? selectedItem.icon() : selectedItem.icon) : ''}
+        <span>{selectedItem.title}</span>
       </div>
       <ul
-        className={`dropdown-list__items-wrapper${
-          listDropped ? " dropdown-list__items-wrapper_dropped" : ""
-        }`}
+        className={listWrapperClassName}
       >
         {items.map(({ title, icon, dataAttributes }) => (
           <li
@@ -53,11 +69,11 @@ export const DropList = ({
             {...dataAttributes}
             onClick={onItemClickHandler}
             className={`dropdown-list__item${
-              selectedItem === title ? " dropdown-list__item_active" : ""
+              selectedItem.title === title ? " dropdown-list__item_active" : ""
             }`}
           >
             {icon ? (typeof icon === "function" ? icon() : icon) : ""}
-            {title}
+            <span>{title}</span>
           </li>
         ))}
       </ul>
